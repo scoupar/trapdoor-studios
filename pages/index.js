984b93f3ee84923ab9e5914df297d33b1788cfb7
Scott Coupar <scott.coupar@googlemail.com>
2021-08-27 21:36:06 +0100

    nav colour

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -9,7 +7,6 @@ export default function Home() {
       <Head>
         <title>Trapdoor Studios</title>
       </Head>
-
       <img className={styles.landingimage} src="/landing-image.jpg" />
       <img className={styles.logo} src="/logo.jpg" />
     </div>
