db50625b838740ac4ddbafd839bcd19cc9d061e7
Scott Coupar <scott.coupar@googlemail.com>
2021-08-27 16:42:18 +0100

    imported fonts

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Trapdoor Studios</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
