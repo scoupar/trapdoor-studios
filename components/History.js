34f29a1c83c44502db37c0ba12ca652d8c5ef3cb
Scott Coupar <scott.coupar@googlemail.com>
2021-08-28 21:34:45 +0100

    added history section

---
+++ components/History.js
@@ -0,0 +1,9 @@
+const History = () => {
+  return (
+    <div className="history-container">
+      <img src="/history-img.jpg" className="history-img" />
+    </div>
+  );
+};
+
+export default History;
--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Bio from "../components/Bio";
+import History from "../components/History";
 
 export default function About() {
   return (
@@ -10,6 +11,7 @@ export default function About() {
       </Head>
       <div className="about-container">
         <Navbar />
+
         <h1 className="title">ABOUT TRAPDOOR</h1>
         <nav className="secondnav">
           <a>BIO</a>
@@ -17,6 +19,7 @@ export default function About() {
           <a>SERVICES</a>
         </nav>
         <Bio />
+        <History />
       </div>
     </>
   );
