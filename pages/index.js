eecce23d843e8941233911ac3b418074fc897dc7
Scott Coupar <scott.coupar@googlemail.com>
2021-08-28 14:50:21 +0100

    changed styling

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
 import Navbar from "../components/Navbar";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,7 +14,11 @@ export default function Home() {
       <div className={styles.logocontainer}>
         <img className={styles.landingimage} src="/landing.jpg" />
         <img className={styles.logo} src="/trapdoor-circle.svg" />
-        <button className={styles.button}>BOOK NOW</button>
+        <Link href="/book">
+          <button className={styles.button}>
+            <a>BOOK NOW</a>
+          </button>
+        </Link>
       </div>
     </div>
   );
