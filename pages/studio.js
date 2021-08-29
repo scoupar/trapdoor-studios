8d696302498c67c72967b0197d9aa5003931bf60
Scott Coupar <scott.coupar@googlemail.com>
2021-08-29 15:20:31 +0100

    added control room component

--- pages/studio.js
+++ pages/studio.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Navbar from "../components/Navbar";
+import Control from "../components/Control";
 
 export default function Studio() {
   return (
@@ -8,12 +9,16 @@ export default function Studio() {
         <title>Trapdoor Studios | Studio</title>
       </Head>
       <div className="studio-container">
-        <Navbar />
-        <h1 className="title">THE STUDIO</h1>
-        <nav className="secondnav">
-          <a>THE CONTROL ROOM</a>
-          <a>THE LIVE ROOM</a>
-        </nav>
+        <div className="studionavcontainer">
+          <Navbar />
+          <h1 className="title">THE STUDIO</h1>
+          <nav className="redsecondnav">
+            <a>THE CONTROL ROOM</a>
+            <a>THE LIVE ROOM</a>
+          </nav>
+        </div>
+
+        <Control />
       </div>
     </>
   );
