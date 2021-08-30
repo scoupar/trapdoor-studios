3ea2fc924b52de1a7c7730db14eedfa11595fb13
Scott Coupar <scott.coupar@googlemail.com>
2021-08-30 19:13:18 +0100

    gear page added

--- pages/gear.js
+++ pages/gear.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Navbar from "../components/Navbar";
+import Outboard from "../components/Outboard";
+import Amplifiers from "../components/Amplifiers";
 
 export default function () {
   return (
@@ -8,15 +10,19 @@ export default function () {
         <title>Trapdoor Studios | Gear</title>
       </Head>
       <div className="gear-container">
-        <Navbar />
-        <h1 className="title">THE GEAR</h1>
-        <nav className="secondnav">
-          <a>OUTBOARD</a>
-          <a>AMPLIFIERS</a>
-          <a>MICROPHONES</a>
-          <a>EFFECTS</a>
-          <a>DRUMS</a>
-        </nav>
+        <div className="gearnavcontainer">
+          <Navbar />
+          <h1 className="title">THE GEAR</h1>
+          <nav className="secondnav">
+            <a>OUTBOARD</a>
+            <a>AMPLIFIERS</a>
+            <a>MICROPHONES</a>
+            <a>EFFECTS</a>
+            <a>DRUMS</a>
+          </nav>
+        </div>
+        <Outboard />
+        <Amplifiers />
       </div>
     </>
   );
