18f81ffc081e9fcf6239a413d45cbf2046f07a1f
Scott Coupar <scott.coupar@googlemail.com>
2021-12-01 00:35:21 +0000

    changed services list

--- components/Services.js
+++ components/Services.js
@@ -5,9 +5,11 @@ const Services = () => {
         <h2>SERVICES</h2>
         <hr></hr>
         <p>Trapdoor offers a number of key services:</p>
-        <p>-Recording and Production</p>
-        <p>-Mixing</p>
-        <p>-Mastering</p>
+        <ul>
+          <li>Recording and Production</li>
+          <li>Mixing</li>
+          <li>Mastering</li>
+        </ul>
         <p>
           Get in touch to discuss your project. We're usually game for anything
           so if you've got something off the beaten track to talk through,
