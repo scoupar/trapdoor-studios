aedbd4b68456c31a47fd1446fd274666da741408
Scott Coupar <scott.coupar@googlemail.com>
2022-01-17 21:55:02 +0000

    added cover art

--- components/Discography.js
+++ components/Discography.js
@@ -5,94 +5,89 @@ import Gallery from "react-grid-gallery";
 const Discography = () => {
   const IMAGES = [
     {
-      src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-      thumbnail:
-        "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
-      thumbnailWidth: 212,
-      thumbnailHeight: 212,
+      src: "/cover-art/vennart.jpg",
+      thumbnail: "/cover-art/vennart.jpg",
+      thumbnailWidth: 300,
+      thumbnailHeight: 300,
       caption: "After Rain (Jeshu John - designerspics.com)",
     },
     {
-      src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-      thumbnail:
-        "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-      thumbnailWidth: 212,
-      thumbnailHeight: 212,
+      src: "/cover-art/amtp-infinity-stare.jpg",
+      thumbnail: "/cover-art/amtp-infinity-stare.jpg",
+      thumbnailWidth: 300,
+      thumbnailHeight: 300,
       caption: "Boats (Jeshu John - designerspics.com)",
     },
 
     {
-      src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-      thumbnail:
-        "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-      thumbnailWidth: 212,
-      thumbnailHeight: 212,
+      src: "/cover-art/last-hyena.jpeg",
+      thumbnail: "/cover-art/last-hyena.jpeg",
+      thumbnailWidth: 300,
+      thumbnailHeight: 300,
     },
     {
-      src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-      thumbnail:
-        "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-      thumbnailWidth: 212,
-      thumbnailHeight: 212,
+      src: "/cover-art/vasa-heroics.jpeg",
+      thumbnail: "/cover-art/vasa-heroics.jpeg",
+      thumbnailWidth: 300,
+      thumbnailHeight: 300,
     },
     {
-      src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-      thumbnail:
-        "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-      thumbnailWidth: 212,
-      thumbnailHeight: 212,
+      src: "/cover-art/bicurious.jpeg",
+      thumbnail: "/cover-art/bicurious.jpeg",
+      thumbnailWidth: 300,
+      thumbnailHeight: 300,
     },
     {
-      src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-      thumbnail:
-        "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-      thumbnailWidth: 212,
-      thumbnailHeight: 212,
+      src: "/cover-art/body-hound.jpeg",
+      thumbnail: "/cover-art/body-hound.jpeg",
+      thumbnailWidth: 300,
+      thumbnailHeight: 300,
     },
     {
-      src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-      thumbnail:
-        "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-      thumbnailWidth: 212,
-      thumbnailHeight: 212,
+      src: "/cover-art/amtp-health.jpeg",
+      thumbnail: "/cover-art/amtp-health.jpeg",
+      thumbnailWidth: 300,
+      thumbnailHeight: 300,
     },
     {
-      src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-      thumbnail:
-        "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-      thumbnailWidth: 212,
-      thumbnailHeight: 212,
+      src: "/cover-art/cleft-wrong.jpeg",
+      thumbnail: "/cover-art/cleft-wrong.jpeg",
+      thumbnailWidth: 300,
+      thumbnailHeight: 300,
     },
     {
-      src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-      thumbnail:
-        "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-      thumbnailWidth: 212,
-      thumbnailHeight: 212,
+      src: "/cover-art/axes.jpeg",
+      thumbnail: "/cover-art/axes.jpeg",
+      thumbnailWidth: 300,
+      thumbnailHeight: 300,
     },
     {
-      src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-      thumbnail:
-        "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-      thumbnailWidth: 212,
-      thumbnailHeight: 212,
+      src: "/cover-art/amtp-droids.jpeg",
+      thumbnail: "/cover-art/amtp-droids.jpeg",
+      thumbnailWidth: 300,
+      thumbnailHeight: 300,
     },
     {
-      src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-      thumbnail:
-        "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-      thumbnailWidth: 212,
-      thumbnailHeight: 212,
+      src: "/cover-art/vasa-colours.jpeg",
+      thumbnail: "/cover-art/vasa-colours.jpeg",
+      thumbnailWidth: 300,
+      thumbnailHeight: 300,
     },
     {
-      src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-      thumbnail:
-        "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-      thumbnailWidth: 212,
-      thumbnailHeight: 212,
+      src: "/cover-art/falls.jpeg",
+      thumbnail: "/cover-art/falls.jpeg",
+      thumbnailWidth: 300,
+      thumbnailHeight: 300,
     },
   ];
-  return <Gallery images={IMAGES} />;
+  return (
+    <Gallery
+      images={IMAGES}
+      margin={0}
+      enableImageSelection={false}
+      enableLightbox={true}
+    />
+  );
 };
 
 export default Discography;
