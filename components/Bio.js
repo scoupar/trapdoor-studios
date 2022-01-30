6035f716abee2e4e5f3cd847e3cb7e4dc1efe6cb
Scott Coupar <scott.coupar@googlemail.com>
2022-01-30 18:47:21 +0000

    about page responsive

--- components/Bio.js
+++ components/Bio.js
@@ -1,24 +1,68 @@
 const Bio = () => {
   return (
     <>
-      <div className="bio-img"></div>
-      <div className="text-container">
-        <h2>BIO</h2>
-        <hr></hr>
+      <div className="about-img">
+        <h2 className="hero-text">
+          Trapdoor Studio is the home of UK Record Producer Tom Peters.
+        </h2>
+      </div>
+      <div className="bio-container">
+        <h2 className="title">BIO</h2>
         <p>
-          Trapdoor Studio is the home of UK Record Producer Tom Peters. Tom has
-          over a decade of working experience in capturing and producing music
-          to the highest standard. Starting out in 2011 as the assistant at The
-          Grand Studio in rural Lancashire, he has spent the last 10 years
-          honing his craft, working in studios all across the UK and Ireland
-          until setting up Trapdoor in April 2019.
-        </p>
-        <p>
+          Tom has over a decade of working experience in capturing and producing
+          music to the highest standard. Starting out in 2011 as the assistant
+          at The Grand Studio in rural Lancashire, he has spent the last 10
+          years honing his craft, working in studios all across the UK and
+          Ireland until setting up Trapdoor in April 2019.
+          <br></br>
+          <br></br>
           Tom's production is vibrant and exciting but still natural and human -
           the emphasis is on making your music sound like the best
           representation of you; full of life, colour and power whilst
           preserving the core values of your creative expression. In other
           words, he wants you to sound uniquely like you.
+        </p>
+      </div>
+      <div className="history-container">
+        <h2 className="title">HISTORY</h2>
+        <p>
+          Trapdoor started as a dream between Tom Peters and Dan Wild-Beesley
+          (guitarist in Cleft/GUG and recording engineer) way back in 2013. The
+          pair were inseparable and spent most of their time together chatting
+          about recording. microphones, bands, amps; everything music related.
+          It was only natural that the conversation would move on to opening a
+          studio together and in 2014, it very nearly happened.
+          <br></br>
+          <br></br>
+          Sadly, things fell through and the studio plan had to take a backseat
+          when in 2015, Dan fell ill. After several years of lengthy treatment,
+          in October 2018, Dan passed away surrounded by his family. He left
+          behind a legacy of incredible joy and happiness and is missed
+          everyday. Trapdoor Studio is a part of that legacy, named after a
+          Cleft song of the same name, it was the best way Tom could think of to
+          remember his best friend by - a sacred shrine to all things Dan and a
+          place where his creative spirit can live on. Dan may not be with us
+          anymore but his presence is felt in every record produced at Trapdoor.
+          <br></br>
+          <br></br>
+          Trapdoor opened its doors in 2019 and was made possible by the
+          generosity of lots of wonderful contributors to the studio's startup
+          Indiegogo campaign. Without them, Trapdoor would not have been
+          possible. Thank you!
+        </p>
+      </div>
+      <div className="services-container">
+        <h2 className="title">SERVICES</h2>
+        <p>
+          Trapdoor offers a number of key services:
+          <ul>
+            <li>Recording and Production</li>
+            <li>Mixing</li>
+            <li>Mastering</li>
+          </ul>
+          Get in touch to discuss your project. We're usually game for anything
+          so if you've got something off the beaten track to talk through,
+          please don't hesitate to get in touch.
         </p>
       </div>
     </>
