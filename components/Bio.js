a398522ca8f5c566d6d0d1195b9ce3b567c8f72e
Scott Coupar <scott.coupar@googlemail.com>
2022-03-27 16:23:02 +0100

    toggled other images

--- components/Bio.js
+++ components/Bio.js
@@ -32,79 +32,98 @@ const BioText = () => {
   );
 };
 
+const HistoryText = () => {
+  return (
+    <section id="history">
+      <div className="history-container">
+        <div className="history-text">
+          <h2>HISTORY</h2>
+          <hr></hr>
+          <p>
+            Trapdoor started as a dream between Tom Peters and Dan Wild-Beesley
+            (guitarist in Cleft/GUG and recording engineer) way back in 2013.
+            The pair were inseparable and spent most of their time together
+            chatting about recording. microphones, bands, amps; everything music
+            related. It was only natural that the conversation would move on to
+            opening a studio together and in 2014, it very nearly happened.
+            <br></br>
+            <br></br>
+            Sadly, things fell through and the studio plan had to take a
+            backseat when in 2015, Dan fell ill. After several years of lengthy
+            treatment, in October 2018, Dan passed away surrounded by his
+            family. He left behind a legacy of incredible joy and happiness and
+            is missed everyday. Trapdoor Studio is a part of that legacy, named
+            after a Cleft song of the same name, it was the best way Tom could
+            think of to remember his best friend by - a sacred shrine to all
+            things Dan and a place where his creative spirit can live on. Dan
+            may not be with us anymore but his presence is felt in every record
+            produced at Trapdoor.
+            <br></br>
+            <br></br>
+            Trapdoor opened its doors in 2019 and was made possible by the
+            generosity of lots of wonderful contributors to the studio's startup
+            Indiegogo campaign. Without them, Trapdoor would not have been
+            possible. Thank you!
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const ServicesText = () => {
+  return (
+    <section id="services">
+      <div className="services-container">
+        <div className="services-text">
+          <h2>SERVICES</h2>
+          <hr></hr>
+          <p>
+            Trapdoor offers a number of key services:
+            <ul>
+              <li>Recording and Production</li>
+              <li>Mixing</li>
+              <li>Mastering</li>
+            </ul>
+            Get in touch to discuss your project. We're usually game for
+            anything so if you've got something off the beaten track to talk
+            through, please don't hesitate to get in touch.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
 const Bio = () => {
-  const [showText, setShowText] = useState(false);
-  const onClick = () => setShowText(!showText);
+  const [showBioText, setShowBioText] = useState(false);
+  const onBioClick = () => setShowBioText(!showBioText);
+  const [showHistoryText, setShowHistoryText] = useState(false);
+  const onHistoryClick = () => setShowHistoryText(!showHistoryText);
+  const [showServicesText, setShowServicesText] = useState(false);
+  const onServicesClick = () => setShowServicesText(!showServicesText);
 
   return (
     <>
       <div className="image-container">
-        <a href="#bio" onClick={onClick}>
+        <a href="#bio" onClick={onBioClick}>
           <Image src={bioImage} className="bio-image" layout="responsive" />
+          <h1 className="imageLink">BIO</h1>
         </a>
       </div>
-      {showText ? <BioText /> : null}
+      {showBioText ? <BioText /> : null}
       <div className="image-container">
-        <Image src={historyImage} className="history-image" />
+        <a href="#history" onClick={onHistoryClick}>
+          <Image src={historyImage} className="history-image" />
+        </a>
       </div>
-      <section id="history">
-        <div className="history-container">
-          <div className="history-text">
-            <h2>HISTORY</h2>
-            <hr></hr>
-            <p>
-              Trapdoor started as a dream between Tom Peters and Dan
-              Wild-Beesley (guitarist in Cleft/GUG and recording engineer) way
-              back in 2013. The pair were inseparable and spent most of their
-              time together chatting about recording. microphones, bands, amps;
-              everything music related. It was only natural that the
-              conversation would move on to opening a studio together and in
-              2014, it very nearly happened.
-              <br></br>
-              <br></br>
-              Sadly, things fell through and the studio plan had to take a
-              backseat when in 2015, Dan fell ill. After several years of
-              lengthy treatment, in October 2018, Dan passed away surrounded by
-              his family. He left behind a legacy of incredible joy and
-              happiness and is missed everyday. Trapdoor Studio is a part of
-              that legacy, named after a Cleft song of the same name, it was the
-              best way Tom could think of to remember his best friend by - a
-              sacred shrine to all things Dan and a place where his creative
-              spirit can live on. Dan may not be with us anymore but his
-              presence is felt in every record produced at Trapdoor.
-              <br></br>
-              <br></br>
-              Trapdoor opened its doors in 2019 and was made possible by the
-              generosity of lots of wonderful contributors to the studio's
-              startup Indiegogo campaign. Without them, Trapdoor would not have
-              been possible. Thank you!
-            </p>
-          </div>
-        </div>
-      </section>
-
+      {showHistoryText ? <HistoryText /> : null}
       <div className="image-container">
-        <Image src={servicesImage} className="services-image" />
+        <a href="#services" onClick={onServicesClick}>
+          <Image src={servicesImage} className="services-image" />
+        </a>
       </div>
-      <section id="services">
-        <div className="services-container">
-          <div className="services-text">
-            <h2>SERVICES</h2>
-            <hr></hr>
-            <p>
-              Trapdoor offers a number of key services:
-              <ul>
-                <li>Recording and Production</li>
-                <li>Mixing</li>
-                <li>Mastering</li>
-              </ul>
-              Get in touch to discuss your project. We're usually game for
-              anything so if you've got something off the beaten track to talk
-              through, please don't hesitate to get in touch.
-            </p>
-          </div>
-        </div>
-      </section>
+      {showServicesText ? <ServicesText /> : null}
     </>
   );
 };
--- pages/about.js
+++ pages/about.js
@@ -13,11 +13,11 @@ export default function About() {
       <HamburgerMenu />
       <div className="aboutnavcontainer">
         <h1 className="title">ABOUT TRAPDOOR</h1>
-        <nav className="secondnav">
+        {/* <nav className="secondnav">
           <a href="#bio">BIO</a>
           <a href="#history">HISTORY</a>
           <a href="#services"> SERVICES</a>
-        </nav>
+        </nav> */}
       </div>
       <div className="about-container">
         <Bio />
