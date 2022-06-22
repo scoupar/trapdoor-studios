f7215299b31aaeaf4830ac5b11408aa3e42c4ed5
Scott Coupar <scott.coupar@googlemail.com>
2022-06-22 12:05:28 +0100

    changed gallery alignment

--- components/Discography.js
+++ components/Discography.js
@@ -9,86 +9,86 @@ const Discography = () => {
     {
       src: "/cover-art/vennart.jpg",
       thumbnail: "/cover-art/vennart.jpg",
-      thumbnailWidth: 200,
-      thumbnailHeight: 200,
+      thumbnailWidth: 150,
+      thumbnailHeight: 150,
       caption: "Vennart - In The Dead, Dead Wood (Engineer - guitars only)",
     },
     {
       src: "/cover-art/amtp-infinity-stare-large.jpeg",
       thumbnail: "/cover-art/amtp-infinity-stare-large.jpeg",
-      thumbnailWidth: 200,
-      thumbnailHeight: 200,
+      thumbnailWidth: 150,
+      thumbnailHeight: 150,
       caption: "Alpha Male Tea Party - Infinity Stare (Produce/Mix)",
     },
 
     {
       src: "/cover-art/last-hyena.jpeg",
       thumbnail: "/cover-art/last-hyena.jpeg",
-      thumbnailWidth: 200,
-      thumbnailHeight: 200,
+      thumbnailWidth: 150,
+      thumbnailHeight: 150,
       caption: "Last Hyena - How Soon is Mars? (Produce/Mix) ",
     },
     {
       src: "/cover-art/vasa-heroics.jpeg",
       thumbnail: "/cover-art/vasa-heroics.jpeg",
-      thumbnailWidth: 200,
-      thumbnailHeight: 200,
+      thumbnailWidth: 150,
+      thumbnailHeight: 150,
       caption: "VASA - Heroics (Produce/Mix)",
     },
     {
       src: "/cover-art/Bicurious.jpeg",
       thumbnail: "/cover-art/Bicurious.jpeg",
-      thumbnailWidth: 200,
-      thumbnailHeight: 200,
+      thumbnailWidth: 150,
+      thumbnailHeight: 150,
       caption: "Bicurious - (re)constructed (Produce/Mix)",
     },
     {
       src: "/cover-art/body-hound.jpeg",
       thumbnail: "/cover-art/body-hound.jpeg",
-      thumbnailWidth: 200,
-      thumbnailHeight: 200,
+      thumbnailWidth: 150,
+      thumbnailHeight: 150,
       caption: "Body Hound - No Moon (Produce/Mix)",
     },
     {
       src: "/cover-art/amtp-health.jpeg",
       thumbnail: "/cover-art/amtp-health.jpeg",
-      thumbnailWidth: 200,
-      thumbnailHeight: 200,
+      thumbnailWidth: 150,
+      thumbnailHeight: 150,
       caption: "Alpha Male Tea Party - Health (Produce/Mix)",
     },
     {
       src: "/cover-art/cleft-wrong.jpeg",
       thumbnail: "/cover-art/cleft-wrong.jpeg",
-      thumbnailWidth: 200,
-      thumbnailHeight: 200,
+      thumbnailWidth: 150,
+      thumbnailHeight: 150,
       caption: "Cleft - Wrong (Engineer)",
     },
     {
       src: "/cover-art/ABAS.jpeg",
       thumbnail: "/cover-art/ABAS.jpeg",
-      thumbnailWidth: 200,
-      thumbnailHeight: 200,
+      thumbnailWidth: 150,
+      thumbnailHeight: 150,
       caption: "A Burial At Sea - A Burial At Sea (Produce/Mix)",
     },
     {
       src: "/cover-art/amtp-droids.jpeg",
       thumbnail: "/cover-art/amtp-droids.jpeg",
-      thumbnailWidth: 200,
-      thumbnailHeight: 200,
+      thumbnailWidth: 150,
+      thumbnailHeight: 150,
       caption: "Alpha Male Tea Party - Droids (Produce/Mix/Master)",
     },
     {
       src: "/cover-art/dog-daisies.jpeg",
       thumbnail: "/cover-art/dog-daisies.jpeg",
-      thumbnailWidth: 200,
-      thumbnailHeight: 200,
+      thumbnailWidth: 150,
+      thumbnailHeight: 150,
       caption: "Dog Daisies - Eagletism (Produce/Mix)",
     },
     {
       src: "/cover-art/falls.jpeg",
       thumbnail: "/cover-art/falls.jpeg",
-      thumbnailWidth: 200,
-      thumbnailHeight: 200,
+      thumbnailWidth: 150,
+      thumbnailHeight: 150,
       caption: "Falls - One Hundred Percent Strong (Produce/Mix)",
     },
   ];
@@ -100,7 +100,7 @@ const Discography = () => {
           <Gallery
             className="gallery"
             images={IMAGES}
-            margin={2}
+            margin={4}
             enableImageSelection={false}
             showImageCount={false}
           />
