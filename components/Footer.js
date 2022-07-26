26634cc1c9e9ac25873b8caf35bc3070d3b6fe4d
Scott Coupar <scott.coupar@googlemail.com>
2022-07-26 23:42:06 +0100

    removed old gallery

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,6 @@ import Gallery from "../components/Gallery";
 const Footer = () => {
   return (
     <>
-      <Gallery />
       <div className="footer">
         <p className="footer-text">© Trapdoor Studios 2022</p>
       </div>
