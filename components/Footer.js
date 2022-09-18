da017d5a52cbae7310d4cdb66b0805e2127314ee
Scott Coupar <scott.coupar@googlemail.com>
2022-09-18 18:48:15 +0100

    added to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,33 @@
-import Gallery from "../components/Gallery";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { SocialIcon } from "react-social-icons";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <>
       <div className="footer">
         <p className="footer-text">© Trapdoor Studios 2022</p>
+        <div className="social-menu">
+          <SocialIcon
+            network="facebook"
+            target="blank"
+            bgColor="white"
+            url="https://www.facebook.com/trapdoorstudiosUK"
+            className="icon"
+            style={{ height: 30, width: 30 }}
+          />
+          <SocialIcon
+            network="instagram"
+            target="blank"
+            bgColor="white"
+            url="https://www.instagram.com/tompetersproduction"
+            className="icon"
+            style={{ height: 30, width: 30 }}
+          />
+        </div>
+        <Link href="http://www.github.com/scoupar">
+          <p className="credit">Wesbite by S. Coupar</p>
+        </Link>
       </div>
     </>
   );
